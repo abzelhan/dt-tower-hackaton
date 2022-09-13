abe81a38d446081df1a576ce145354528d03bb55
abzelhan <abzalsahitov@gmail.com>
2022-09-13 14:33:50 +0300

    Project skeleton

---
+++ src/main/java/com/ama/tower/DatsTeamTowerHackatonApplication.java
@@ -0,0 +1,13 @@
+package com.ama.tower;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatsTeamTowerHackatonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatsTeamTowerHackatonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ama/tower/DatsTeamTowerHackatonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ama.tower;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatsTeamTowerHackatonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
